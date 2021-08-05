e6a6f219848efa73b6084d0a91b706a4c2289822
Ishaan <agrawalishaan115@gmail.com>
2021-08-05 20:42:27 +0530

    My wrk for Day 5

---
+++ for loop.py
@@ -0,0 +1,4 @@
+fruits = ["Apple", "Peach", "Pear"]
+for eat in fruits:
+  print(eat)
+  print(eat + " Pie")
---
+++ range function.py
@@ -0,0 +1,6 @@
+# for number in range(0, 13, 3):
+#   print(number)
+total = 0
+for number in range(1, 101):
+  total += number
+print(total)
